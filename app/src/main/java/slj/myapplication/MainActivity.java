f24de0c131169fa005a2f61a11859724ce212f0a
kaduyayoshida <sougo0310@yahoo.co.jp>
2015-05-31 18:51:54 +0900

    ゲームアクティビティ追加

--- app/src/main/java/slj/myapplication/MainActivity.java
+++ app/src/main/java/slj/myapplication/MainActivity.java
@@ -1,38 +1,42 @@
+
 package slj.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
-public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public class MainActivity extends Activity implements OnClickListener {
+        /**
+         * Called when the activity is first created.
+         */
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        private Button button_segue;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            button_segue = (Button) findViewById(R.id.button_segue);
+            button_segue.setOnClickListener(this);
+
         }
 
-        return super.onOptionsItemSelected(item);
+        public void onClick(View v) {
+            if (v == button_segue) {
+                Intent intent = new Intent(this, Game_Activity.class);
+                startActivityForResult(intent, 0);
+
+            }
+        }
     }
-}
+
