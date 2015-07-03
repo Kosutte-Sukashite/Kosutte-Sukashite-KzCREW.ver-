05fbe11315c6dc201906fa6070d708ebb2f09256
kaduyayoshida <sougo0310@yahoo.co.jp>
2015-07-03 20:24:27 +0900

    多方向

--- app/src/main/java/slj/myapplication/Game_Activity.java
+++ app/src/main/java/slj/myapplication/Game_Activity.java
@@ -7,6 +7,8 @@ import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -16,7 +18,8 @@ public class Game_Activity extends Activity {
     private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 
     private GestureDetector mGestureDetector;
-
+    private int mCount;
+    private View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,21 +40,41 @@ public class Game_Activity extends Activity {
 
             try {
 
-                if (Math.abs(event1.getY() - event2.getY()) > SWIPE_MAX_OFF_PATH) {
-                    // 縦の移動距離が大きすぎる場合は無視
-                    return false;
-                }
+                //if (Math.abs(event1.getY() - event2.getY()) > SWIPE_MAX_OFF_PATH) {
+                // 縦の移動距離が大きすぎる場合は無視
+                //  return false;
+                //今回のゲームでは咆哮指定等はしないため、削除
+                // }
 
                 if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
                     // 開始位置から終了位置の移動距離が指定値より大きい
                     // X軸の移動速度が指定値より大きい
                     Toast.makeText(Game_Activity.this, "右から左", Toast.LENGTH_SHORT).show();
-
+                    //こすった回数をカウント
+                    plus(view);
                 } else if (event2.getX() - event1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
                     // 終了位置から開始位置の移動距離が指定値より大きい
                     // X軸の移動速度が指定値より大きい
-                    Toast.makeText(Game_Activity .this, "左から右", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Game_Activity.this, "左から右", Toast.LENGTH_SHORT).show();
+                    //こすった回数をカウント
+                    plus(view);
                 }
+
+                if (event1.getY() - event2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    // 開始位置から終了位置の移動距離が指定値より大きい
+                    // Y軸の移動速度が指定値より大きい
+                    Toast.makeText(Game_Activity.this, "下から上", Toast.LENGTH_SHORT).show();
+                    //こすった回数をカウント
+                    plus(view);
+
+                } else if (event2.getY() - event1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    // 終了位置から開始位置の移動距離が指定値より大きい
+                    // Y軸の移動速度が指定値より大きい
+                    Toast.makeText(Game_Activity.this, "上から下", Toast.LENGTH_SHORT).show();
+                    //こすった回数をカウント
+                    plus(view);
+                }
+
 
             } catch (Exception e) {
                 // nothing
@@ -85,6 +108,17 @@ public class Game_Activity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void plus(View view) {
+        //こすった数だけ読み込まれる。
+        this.view = view;
+        mCount++;
+        TextView countView = (TextView) findViewById(R.id.textView1);
+        //activity_game_.xmlの表示を増やす
+        countView.setText(String.valueOf(mCount));
+    }
+
+
 
 
 
