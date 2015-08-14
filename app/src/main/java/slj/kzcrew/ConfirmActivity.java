4dcf54306e99364720fa5adb7c287b89d2401584
Tomi <yuuichi10412145@gmail.com>
2015-08-14 22:36:40 +0900

    画像追加

---
+++ app/src/main/java/slj/kzcrew/ConfirmActivity.java
@@ -0,0 +1,39 @@
+package slj.kzcrew;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class ConfirmActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.backButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+                // 次画面のアクティビティ起動
+                startActivity(intent);
+            }
+        });
+
+        //weページリンク作成
+       ImageButton linkBtn = (ImageButton)findViewById(R.id.linkButton);
+       linkBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://peatix.com/event/105262/"));
+               startActivity(intent);
+           }
+       });
+
+    }
+}
